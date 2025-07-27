305bcdcb0fb21ee520d26936388b6ab13f11eebd
Mantu Kumar Singh <162816910+MantukumarSingh@users.noreply.github.com>
2025-07-27 02:51:55 +0000

    variables.js added

---
+++ 01-Basics/01_variables.js
@@ -0,0 +1,26 @@
+/*
+Variables are used to store data . 
+Data is nothing it is collection of information . 
+There are three types of variables in javaScript .
+
+const, let , var 
+
+By suing this we can store  Data , which will allocate some memory space to allocate date with variable  name .
+Variable name is always start with small letter.
+
+Let's see ex:-
+*/
+
+const accountId = 12345;
+let accountType = "saving";
+let accountNumber = 12345678901;
+var emailId = "mantu@gmail.com"
+let  mobileNumber = 9122986734;
+let accountState ;
+let accountCity ; 
+console.log(accountId);
+console.log(accountType);
+console.log(accountNumber);
+console.log(accountState);
+
+console.table([accountId,accountType,accountNumber,emailId,mobileNumber,accountState,,accountCity]);

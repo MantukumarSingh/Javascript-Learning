e37793c6c9dab767d1ebab47d72b40e431aafc1e
Mantu Kumar Singh <162816910+MantukumarSingh@users.noreply.github.com>
2025-07-31 17:38:09 +0000

    types of different datatypes

---
+++ 01-Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+/* Datatypes are of  different types in javaScript 
+=>
+    Number, bigInt , String , Boolean ,
+*/
+console.log(3+3);
+// console.log(3+3) console.log(5+6);
+// SyntaxError: Unexpected identifier 'console'
+console.log(3+3); console.log(7+8);
+// It will give output give output but the the coding standard matter  , readable , Have some coding standard .
+
+let score = 33;
+console.log(score);
+
+
+let name = "Mantu";
+console.log(name);
+
+let isloggedIn = "false";
+console.log(isloggedIn);
+
+let phoneNumber = null;
+console.log(phoneNumber);
+// When you declare a variable and assign null to it , it is null
+
+let address;
+console.log(address); 
+// address is no
+// when you declare a variable but not assign any value to it , it is undefined.
+
+
+

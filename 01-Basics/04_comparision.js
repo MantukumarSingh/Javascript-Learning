e758ef92164b058008b1556a8cd95a2bbb4bacc2
Mantu Kumar Singh <162816910+MantukumarSingh@users.noreply.github.com>
2025-08-04 19:49:54 +0000

    undefined comparision explaination added

--- 01-Basics/04_comparision.js
+++ 01-Basics/04_comparision.js
@@ -73,3 +73,20 @@ This is why null >= 0 evaluates to true, while null > 0 evaluates to false
 
 /* The reason is that an equality check == and compariosn > , < , >= , <= work differently . */
 /* Comparison convert null to a number, treating it as 0. That's why null >= 0 and null > 0 is false */
+
+
+/* comparion of undefined  */
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined >= 0); // false
+console.log(undefined == 0); // false
+console.log(undefined < 1); // false
+console.log(undefined <= 1); // false
+console.log(undefined == 1); // false
+/* explaination = "In JavaScript, undefined is a special value that represents the absence of a value or an uninitialized variable.
+   When comparing undefined with a number, JavaScript treats undefined as NaN (Not-a-Number) for the purpose of comparison.
+   This is why all comparisons involving undefined and numbers return false,
+   except for strict equality checks (===) which will also return false."; 
+*/
+console.log(undefined === 1);
+ 

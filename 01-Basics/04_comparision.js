de17a23fcbe8a29c6644540561837f40c881f1e0
Mantu Kumar Singh <mantusingh0708@gmail.com>
2025-08-05 00:26:20 +0530

    comparision file added

---
+++ 01-Basics/04_comparision.js
@@ -0,0 +1,36 @@
+// let num1 = 10;
+// let num2 = 20;
+// // console.log( num1 = num2);
+// console.log( num1 == num2);
+
+// console.log( 2 > 3);
+// console.log( 2 < 3);
+// console.log(null == 1);
+// console.log(null == null);
+// console.log(typeof null);
+// console.log(typeof undefined);
+// console.log(typeof(null));
+// console.log(typeof(Boolean(null)));
+// console.log(typeof(Boolean));
+// console.log("2" > 1);
+// console.log(null == 1);
+
+
+// console.log(null == 0);
+// console.log(null == 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log( null > 1 );
+// console.log( null <= 1);
+// console.log( null < 1 );
+console.log(null == 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// === 
+
+// console.log("2" === 2);
